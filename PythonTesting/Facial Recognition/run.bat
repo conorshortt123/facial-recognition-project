886b6478c42496911ede01c31e499e9a23358e7a
CONOR SHORTT <G00360253@gmit.ie>
2020-02-18 15:30:26 +0000

    Update

--- PythonTesting/Facial Recognition/run.bat
+++ PythonTesting/Facial Recognition/run.bat
@@ -1,3 +1,2 @@
-"C:\Users\Conor Shortt\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\Conor Shortt\Desktop\ProfITProj\facial-recognition-project\PythonTesting\Facial Recognition\image_capture.py"
-pause
-"C:\Users\Conor Shortt\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\Conor Shortt\Desktop\ProfITProj\facial-recognition-project\PythonTesting\Facial Recognition\facial_recognition.py"
+python image_capture.py
+python facial_recognition.py
