dd7d250a0bea24d0d29510d6721bae164c6aefa1
Mark Reilly <37144811+MarkReillyGMIT@users.noreply.github.com>
2020-02-25 14:28:30 +0000

    Fix Error when registering with database

--- FrontEnd/prototype/forms.py
+++ FrontEnd/prototype/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from prototype.models import User
 
 
 class RegistrationForm(FlaskForm):
