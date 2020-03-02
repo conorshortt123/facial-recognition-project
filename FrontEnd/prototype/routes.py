a8e94afe57622e0d8cda56f6ff3fa654b21b3bd6
blaine burke <blaineburke1999@gmail.com>
2020-03-02 12:37:52 +0000

    Login/Register Verification working

--- FrontEnd/prototype/routes.py
+++ FrontEnd/prototype/routes.py
@@ -38,85 +38,120 @@ posts = [
 def home():
     return render_template('home.html', posts=posts)
 
+#_______________________________________________________________________________________________________________
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-
-
+#_______________________________________________________________________________________________________________
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-
-
+    #Get registration form 
     form = RegistrationForm()
 
-
+    #Validate Form Username to ensure no username is the same
+    #Therefore making usernames a primary Key for the database
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
 
         found = False
-        searchName = form.username.data
-        print("searchName " + searchName)
-        results = collection.find({"userName":searchName})
+        searchEmail = form.email.data
+        print("searchEmail " + searchEmail)
+        #Search database for the entered username 
+        results = collection.find({"email":searchEmail})
         for result in results:
             print(result)
-            if result["userName"] == searchName:
-                found = True
-                print("UserName Found")
-                print(result["userName"])
 
+            # If found the 'found' variable becomes true it fails the 
+            # registration attempt as the username is taken
+            if result["email"] == searchEmail:
+                found = True
+                print("User Found")
+                print(result["email"])
+
+        # If not found the 'found' variable remains false and 
+        # the registration succeeds as the username isn't taken
         if found == False:
             #Add details to mongoDB database 
             post = {"userName":form.username.data,"password":form.password.data,"email":form.email.data}
             collection.insert_one(post)
             flash('Your account has been created! You are now able to log in', 'success')
+            # if it is successful you are returned home
             return redirect(url_for('home'))
         else:
-            flash('Username already Taken!', 'Error')
+            flash('email already Taken!', 'error')
 
-
+    # if it is unsuccessful you are not moved
     return render_template('register.html', title='Register', form=form)
+
+#_______________________________________________________________________________________________________________
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+
+
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
 
-        results = collection.find({"email":form.email.data})
-        print(results)
+        found = False
+        searchEmail = form.email.data
+        searchPass = form.password.data
+        print("searchEmail " + searchEmail)
+        #Search database for the entered username 
+        results = collection.find({"email":searchEmail})
+        for result in results:
+            print(result)
 
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            if result["email"] == searchEmail:
+                print("Email Found")
+                if result["password"]==searchPass:
+                    found = True
+
+        if found == True:
+            flash('You are now logged in!', 'success')
+            return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check Email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+    #     if user and bcrypt.check_password_hash(user.password, form.password.data):
+    #         login_user(user, remember=form.remember.data)
+    #         next_page = request.args.get('next')
+    #         return redirect(next_page) if next_page else redirect(url_for('home'))
+    #     else:
+    #         flash('Login Unsuccessful. Please check username and password', 'danger')
+    # return render_template('login.html', title='Login', form=form)
+
+#_______________________________________________________________________________________________________________
 
 @app.route("/face_recognition", methods=['GET', 'POST'])
 def facerecognition():
    return render_template('videofeed.html', title='Facial Recognition')
+
+#_______________________________________________________________________________________________________________
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#_______________________________________________________________________________________________________________
 
 @app.route("/account")
 @login_required
 def account():
     return render_template('account.html', title='Account')
 
+#_______________________________________________________________________________________________________________
 
 @app.route("/video_feed")
 def video_feed():
